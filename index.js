8bde786b43e6d93dd9b1e92fbfcf627d95b4b0fc
Zorenkoi <zorenkoi956@gmail.com>
2023-10-30 14:41:13 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+const PORT = 4444
+
+app.get('/', (req, res) => {
+  res.end(`<h1>MY APP Bitch</h1>`)
+})
+
+app.listen(PORT, () => {
+  console.log('server pashet na portu: 4000')
+})
