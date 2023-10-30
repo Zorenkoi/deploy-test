4e459901a34d7b21c1cb88963fc29fa8fde231e2
Zorenkoi <zorenkoi956@gmail.com>
2023-10-30 14:55:54 +0200

    add home route

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const PORT = 4444
 app.get('/', (req, res) => {
   res.end(`<h1>MY APP Bitch</h1>`)
 })
+app.get('/home', (req, res) => {
+  res.end(`<h1>MY APP Bitch homik</h1>`)
+})
 
 app.listen(PORT, () => {
   console.log('server pashet na portu: 4000')
